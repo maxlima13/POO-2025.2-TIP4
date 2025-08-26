fb13dffa4d617930311b54977d972d20e8bdc27c
Max Lima <max.lima11@aluno.ifce.edu.br>
2025-08-25 21:16:45 -0300

    Add files via upload

---
+++ Lista-01/Exercicios-Propostos/Q01/src/br/edu/principal/Principal.java
@@ -0,0 +1,14 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// declaração dos números
+		int num1 = 8;
+        int num2 = 2;
+
+        // saída já com a subtração 
+        System.out.println("O resultado da subtração é: " + (num1 - num2));
+	}
+
+}
