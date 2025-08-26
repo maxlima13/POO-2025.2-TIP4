9271ecec1751807f6ada32cb6a59713d095b46ca
Max Lima <max.lima11@aluno.ifce.edu.br>
2025-08-25 21:18:40 -0300

    Add files via upload

---
+++ Lista-01/Exercicios-Propostos/Q02/src/br/edu/principal/Principal.java
@@ -0,0 +1,18 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// declaração dos números
+		int num1 = 1;
+	    int num2 = 2;
+	    int num3 = 3;
+	    
+	    // saída já com a multiplicação
+	    System.out.println("O resultado da multiplicação é: " + (num1 * num2 * num3));
+	    
+	    
+
+	}
+
+}
