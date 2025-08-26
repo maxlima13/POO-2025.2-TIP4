b7e47a427deb8ef57578b616593c5e3b22758762
Max Lima <max.lima11@aluno.ifce.edu.br>
2025-08-25 21:20:49 -0300

    Add files via upload

---
+++ Lista-01/Exercicios-Propostos/Q03/src/br/edu/principal/Principal.java
@@ -0,0 +1,17 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// declaração dos números
+		int num1 = 2;
+		int num2 = 2;
+		
+		//saída já com a divisão
+		System.out.println("O resultado da divisão é: " + (num1 / num2));
+		
+		
+
+	}
+
+}
